c0b593dd0680795e5c9a193baa5ba6514d887313
clickTwice26 <shagatoc@gmail.com>
2023-08-26 19:33:49 +0600

    token changed

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 url = "https://discord.com/api/oauth2/authorize?client_id=1144865343567822889&permissions=2183991392320&scope=bot"
-token = "MTE0NDg2NTM0MzU2NzgyMjg4OQ.Gqe_5h.NNB2E4PQVQ5gOix4kyPL2gyzmH-OPQQaclfBr4"
+token = "MTE0NDg2NTM0MzU2NzgyMjg4OQ.GsFxdF.Ry3--BWwHOQCHhBurml7j5lUZA574WXbvVj7lk"
 api_prefix = "https://api.mcstatus.io/v2/status/java/"
 site = "play.freakmcworld.tk"
