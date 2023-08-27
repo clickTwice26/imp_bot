d30cfedbc383d52cf8fc6d69825fa4d06ab13e22
clickTwice26 <shagatoc@gmail.com>
2023-08-27 06:01:44 +0600

    constants fixed

--- constants.py
+++ constants.py
@@ -2,7 +2,6 @@
 from dotenv import dotenv_values
 token = dotenv_values(".env")["TOKEN"]
 
-url = "https://discord.com/api/oauth2/authorize?client_id=1144865343567822889&permissions=2183991392320&scope=bot"
 
 api_prefix = "https://api.mcstatus.io/v2/status/java/"
 site = open("site.txt", "r").read()
