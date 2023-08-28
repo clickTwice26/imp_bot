9787b96c31b72763c5cc5b4a4b714863a7580e11
clickTwice26 <shagatoc@gmail.com>
2023-08-28 06:11:26 +0600

    token reseter added

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-try:
-    import requests
-    import discord
-    import dotenv
-except ModuleNotFoundError:
-    os.system("pip3 install -r requrirements.txt")
-    os.system("pip install -r requirements.txt")     
-import bot
-import func as f
-import os
 import sys
-if __name__ == "__main__":
-    f.sitechecker()
-    #run the bot
-    f.firsttimetokenchecker()
+try:
+    resetoken = sys.argv[1]
+    if resetoken == "reset":
+        with open(".env","w") as tokenreseter:
+            tokenreseter.write("TOKEN = ")
+            tokenreseter.close()
+        print("Token reset done")
 
 
-       
-    # sys.exit("Testing")
-    bot.run_discord_bot()
+except IndexError:
+    try:
+        import requests
+        import discord
+        import dotenv
+    except ModuleNotFoundError:
+        os.system("pip3 install -r requrirements.txt")
+        os.system("pip install -r requirements.txt")     
+    import bot
+    import func as f
+    import os
+    import sys
+    if __name__ == "__main__":
+        f.sitechecker()
+        #run the bot
+        f.firsttimetokenchecker()
+
+
+        
+        # sys.exit("Testing")
+        bot.run_discord_bot()
